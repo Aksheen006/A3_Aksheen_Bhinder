b386cc36fb8057abd0e4b844f886d2e2b8b64e88
Aksheen Bhinder <bhindeak@sheridancollege.ca>
2025-04-02 16:04:24 -0400

    Intial commit

---
+++ src/main/java/ca/sheridancollege/bhindeak/a3_aksheen_bhinder/A3AksheenBhinderApplication.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.bhindeak.a3_aksheen_bhinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A3AksheenBhinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A3AksheenBhinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/sheridancollege/bhindeak/a3_aksheen_bhinder/A3AksheenBhinderApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.bhindeak.a3_aksheen_bhinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A3AksheenBhinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
